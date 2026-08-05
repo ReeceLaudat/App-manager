adb70826eebecaabdbcf5ac8fe4ded06980145c3
Reece Laudat <45715991+ReeceLaudat@users.noreply.github.com>
2026-08-05 22:51:23 +0000

    Adjusting layout

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MainApp(ttk.App):
 		super().__init__()
 		self.theme_use('catppuccin-dark')
 		self.title("App Manager")
-		self.geometry("700x450")
+		self.geometry("1000x650")
 		self.style.configure('long.TNotebook', tabposition='wn')
 
 		self.button = ttk.Button(self, text="Button")
@@ -33,17 +33,22 @@ class NotebookTabs(ttk.Notebook):
 class PasswordGen(ttk.Frame):
 	def __init__(self, parent):
 		super().__init__(parent)
-		
 		parent.add(self, text='Password Genarator')
-	
 
-		#self.pack(side=RIGHT, padx=10, pady=20, fill='both')
+		#Styling of frame in tabe
+		self['padding'] = (5,10,5,10)
+		self['borderwidth'] = 5
+		self['relief'] = 'flat'
 
-		self.buttonA = ttk.Button(self, text='Button A')
-		self.buttonB = ttk.Button(self, text='Button B')
+		#Layout
+		self.columnconfigure(0, weight=1)
+		self.columnconfigure(0, weight=3)
 
-		self.buttonA.grid(row=0, column=0, padx=10)
-		self.buttonB.grid(row=1, column=0, padx=10)
+		#self.grid(padx=20, pady=20)
+		ttk.Label(parent, text='Word to use for \npassword generation').grid(row=1, column=2, padx=50, pady=20)
+		self.PasswordEntry = ttk.Entry(parent)
+
+		self.PasswordEntry.grid(row=1, column=3, padx=10)
 
 class ProductFinder(ttk.Frame):
 	def __init__(self, parent):
@@ -51,7 +56,7 @@ class ProductFinder(ttk.Frame):
 		
 		#self.pack(side=LEFT, padx=20, pady=20)
 
-		parent.add(self, text='ProductFinder')
+		parent.add(self, text='Product Finder')
 
 		self.buttonA = ttk.Button(self, text='Button A')
 		self.buttonB = ttk.Button(self, text='Button B')
