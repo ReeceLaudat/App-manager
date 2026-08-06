26cbc8078664429a6dfbada3ce73a41ff9c3b133
Reece Laudat <45715991+ReeceLaudat@users.noreply.github.com>
2026-08-06 16:02:14 +0000

    Fixed visual bug and added text field

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ class MainApp(ttk.App):
 		NotebookTabs(self)
 
 class NotebookTabs(ttk.Notebook):
-	def __init__(self, parent):
-		super().__init__(parent, style='long.TNotebook')
+	def __init__(self, parent, **kwargs):
+		super().__init__(parent, **kwargs, style='long.TNotebook')
 
-		self.pack(fill=BOTH, expand=YES, padx=20, pady=20)
+		self.pack(fill='both', expand=True, padx=20, pady=20, ipadx=20, ipady=20)
 
 
 		PasswordGen(self)
@@ -33,36 +33,46 @@ class NotebookTabs(ttk.Notebook):
 class PasswordGen(ttk.Frame):
 	def __init__(self, parent):
 		super().__init__(parent)
-		parent.add(self, text='Password Genarator')
+		parent.add(self, text='Password \nGenarator')
 
-		#Styling of frame in tabe
-		self['padding'] = (5,10,5,10)
-		self['borderwidth'] = 5
-		self['relief'] = 'flat'
+
+
 
 		#Layout
-		self.columnconfigure(0, weight=1)
-		self.columnconfigure(0, weight=3)
+		#self.grid(ipadx=20, ipady=40)
+		self.columnconfigure((0,1,2,3,4,5), weight=1, uniform='a')
 
-		#self.grid(padx=20, pady=20)
-		ttk.Label(parent, text='Word to use for \npassword generation').grid(row=1, column=2, padx=50, pady=20)
-		self.PasswordEntry = ttk.Entry(parent)
 
-		self.PasswordEntry.grid(row=1, column=3, padx=10)
+		self.rowconfigure((0,1,2,3,4,5), weight=1, uniform='a')
+
+
+
+		#widgets
+		self.gen_label = ttk.Label(self, width=20, text='Word for \npassword generation', relief='flat'
+			).grid(row=0, column=1, padx=10, pady=10)
+
+		self.passwordEntry = ttk.Entry(self
+			).grid(row=0, column=2)
+
+		self.generate_button = ttk.Button(self, text='Generate'
+			).grid(row=0, column=3)
+
+		self.output_text = ttk.Text(self
+			).grid(row=2, column=1)
 
 class ProductFinder(ttk.Frame):
 	def __init__(self, parent):
 		super().__init__(parent)
 		
-		#self.pack(side=LEFT, padx=20, pady=20)
+		self.grid(padx=20, pady=40)
 
-		parent.add(self, text='Product Finder')
+		parent.add(self, text='Product \nFinder')
 
 		self.buttonA = ttk.Button(self, text='Button A')
 		self.buttonB = ttk.Button(self, text='Button B')
 
-		self.buttonA.grid(row=0, column=0, padx=10)
-		self.buttonB.grid(row=1, column=0, padx=10)
+		self.buttonA.grid(row=0, column=0, padx=10, pady=10)
+		self.buttonB.grid(row=1, column=0, padx=10, pady=10)
 
 	
 
